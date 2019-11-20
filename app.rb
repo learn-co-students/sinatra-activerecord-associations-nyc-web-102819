9d1c56c9422a390880c55a5ac271f68940a2a420
Barak Saidoff <barak.saidoff@gmail.com>
2019-11-20 07:52:37 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    Cat.create(:name => "Maru", :age => 3, :breed => "Scottish Fold")
+    Cat.create(:name => "Hannah", :age => 2, :breed => "Tabby")
+    Cat.create(:name => "Patches", :age => 2, :breed => "Calico")
 end
